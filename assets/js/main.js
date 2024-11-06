7485cd3e162f12284450a281bfcf7a91ff2e5726
marynaKolikova <marynakolikova@gmail.com>
2024-11-06 19:28:42 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,40 +6,44 @@ const radioButtons = document.querySelectorAll('.form__radio-input');
 const billInput = document.getElementById("bill");
 const tipRadios = document.getElementsByName("tip");
 const peopleInput = document.getElementById("number-of-people");
-let billAmount;
+let billAmount = 0;
 let peopleAmount;
 
 const tipAmountPerPerson = document.getElementById("tip-amount");
 const totalAmountPerPersonAmount = document.getElementById("total-amount");
 const resetButton = document.getElementById("reset");
+const emailHint = document.querySelector('#valid-email-required');
+const formBillWrapper = document.querySelector('.form__bill-wrapper');
+
+const wrapper = peopleInput.closest('.form__bill-wrapper')
 
 radioWrappers.forEach(wrapper => {
-    wrapper.addEventListener('click', () => {
-        const input = wrapper.querySelector('input[type="radio"]');
-        if (input) {
-            input.checked = true;
-            input.dispatchEvent(new Event('change'));
-        }
-    });
+  wrapper.addEventListener('click', () => {
+    const input = wrapper.querySelector('input[type="radio"]');
+    if (input) {
+      input.checked = true;
+      input.dispatchEvent(new Event('change'));
+    }
+  });
 });
 
 radioButtons.forEach(input => {
-    input.addEventListener('change', () => {
-        radioWrappers.forEach(wrapper => {
-            wrapper.classList.remove('selected');
-        });
-
-        const selectedWrapper = input.closest('.form__radio-w');
-        if (selectedWrapper) {
-            selectedWrapper.classList.add('selected');
-        }
+  input.addEventListener('change', () => {
+    radioWrappers.forEach(wrapper => {
+      wrapper.classList.remove('selected');
     });
+
+    const selectedWrapper = input.closest('.form__radio-w');
+    if (selectedWrapper) {
+      selectedWrapper.classList.add('selected');
+    }
+  });
 });
 
 customInput.addEventListener('focus', () => {
-    radioWrappers.forEach(wrapper => {
-        wrapper.classList.remove('selected');
-    });
+  radioWrappers.forEach(wrapper => {
+    wrapper.classList.remove('selected');
+  });
 });
 
 ////////////////////////////////////////////////////////////////////////
@@ -50,7 +54,7 @@ function getSelectedTip() {
       selectedTip = parseFloat(radio.value);
     }
   });
-  
+
   if (!selectedTip && customInput.value) {
     selectedTip = parseFloat(customInput.value);
   }
@@ -60,11 +64,17 @@ function getSelectedTip() {
 function handlePeopleInput() {
   billAmount = parseFloat(billInput.value);
   peopleAmount = parseInt(peopleInput.value);
+  const wrapper = peopleInput.closest('.form__bill-wrapper')
 
   if (!isNaN(peopleAmount) && peopleAmount > 0) {
     calculate();
+    emailHint.style.visibility = 'hidden';
+    wrapper.classList.remove('border-red');
   } else {
-    console.log("Введіть коректну кількість людей");
+    emailHint.style.visibility = 'visible';
+    wrapper.classList.add('border-red');
+    tipAmountPerPerson.textContent = '$0.00';
+    totalAmountPerPersonAmount.textContent = '$0.00';
   }
 }
 
@@ -96,3 +106,4 @@ function resetCalculator() {
 }
 
 resetButton.addEventListener("click", resetCalculator);
+
